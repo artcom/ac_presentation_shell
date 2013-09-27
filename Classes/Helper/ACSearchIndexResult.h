3fce04b0969f04208a674eae333a12409b6c16f0
Patrick Juchli <patrickjuchli@gmail.com>
2013-09-27 23:02:11 +0200

    A search result is wrapped in ACSearchIndexResult

---
+++ Classes/Helper/ACSearchIndexResult.h
@@ -0,0 +1,15 @@
+//
+//  ACSearchIndexResult.h
+//  ACShell
+//
+//  Created by Patrick Juchli on 27.09.13.
+//  Copyright (c) 2013 ART+COM AG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ACSearchIndexResult : NSObject
+@property (nonatomic, assign) SKDocumentID documentId;
+@property (nonatomic, retain) NSURL *documentUrl;
+@property (nonatomic, assign) float score;
+@end
---
+++ Classes/Helper/ACSearchIndexResult.m
@@ -0,0 +1,13 @@
+//
+//  ACSearchIndexResult.m
+//  ACShell
+//
+//  Created by Patrick Juchli on 27.09.13.
+//  Copyright (c) 2013 ART+COM AG. All rights reserved.
+//
+
+#import "ACSearchIndexResult.h"
+
+@implementation ACSearchIndexResult
+
+@end
