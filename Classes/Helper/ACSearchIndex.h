62cee2fb40660385a025b6d2f422f8bbbe0f733d
Patrick Juchli <patrickjuchli@gmail.com>
2013-10-10 17:57:48 +0200

    Better comments

--- Classes/Helper/ACSearchIndex.h
+++ Classes/Helper/ACSearchIndex.h
@@ -17,11 +17,13 @@ typedef void (^ACSearchResultBlock)(NSArray *results);
 
 /**
  Asynchronous full-text search using Search Kit
- All method are executed asynchronously but enqueued in a single serial queue, thus calling two methods in direct
+ 
+ All methods are executed asynchronously but enqueued in a single serial queue, thus calling two methods in direct
  succession is fine. E.g. addDocumentsAt: immediately followed by search: will work as expected, search will
  be executed after addDocumentsAt: has finished.
  Because of this house-keeping this class is opaque and can't hand out a reference to the internally
  used SKIndexRef.
+ NOTE: Methods have to be called on the main thread.
  */
 @interface ACSearchIndex : NSObject
 
@@ -29,17 +31,16 @@ typedef void (^ACSearchResultBlock)(NSArray *results);
 
 /**
  Add a document to the index
- @param path to the document
- @param update the index automatically after the operation
- @param a completion block called after the operation
+ @param path File path to the document
+ @param completion Block called after the operation
  */
 - (void)addDocumentAt:(NSString *)path completion:(void(^)())completionBlock;;
 
 /**
  Index all documents in a folder and its sub-folders that have a given extension
- @param path to a folder
- @param extenstion to be required
- @param a completion block called after documents have been added
+ @param path File path to a folder
+ @param extension Method will add all documents using a file @a extension
+ @param completion Block called after documents have been added
  */
 - (void)addDocumentsAt:(NSString *)path withExtension:(NSString *)extension completion:(void(^)(NSInteger numDocuments))completionBlock;
 
@@ -47,8 +48,9 @@ typedef void (^ACSearchResultBlock)(NSArray *results);
  Searches the index using a query string
  See docs for SKSearchCreate for query string features (phrase searching is enabled):
  https://developer.apple.com/library/mac/documentation/userexperience/Reference/SearchKit/Reference/reference.html#jumpTo_51
- @param a query string
- @param a completion handler with an array of ACSearchIndexResult
+ @param query Query string
+ @param maxNumResults The maximum number of results to be collected and returned
+ @param completion Block called with an NSArray of ACSearchIndexResult instances
  @return a cancelable NSOperation subclass encapsulating the search
  @see ACSearchIndexQuery
  @see ACSearchIndexResult
