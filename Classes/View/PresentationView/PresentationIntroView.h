58c13620225837a921472ef303f9959047fc52cd
Julian Krumow <julian.krumow@artcom.de>
2016-03-03 14:27:57 +0100

    Add presentation intro view.

---
+++ Classes/View/PresentationView/PresentationIntroView.h
@@ -0,0 +1,13 @@
+//
+//  PresentationIntroView.h
+//  ACShell
+//
+//  Created by Julian Krumow on 03.03.16.
+//  Copyright © 2016 ART+COM AG. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PresentationIntroView : NSView
+
+@end
---
+++ Classes/View/PresentationView/PresentationIntroView.m
@@ -0,0 +1,14 @@
+//
+//  PresentationIntroView.m
+//  ACShell
+//
+//  Created by Julian Krumow on 03.03.16.
+//  Copyright © 2016 ART+COM AG. All rights reserved.
+//
+
+#import "PresentationIntroView.h"
+
+@implementation PresentationIntroView
+
+
+@end
